4aeb92aa2b6743f1ff3783989e071c59404743eb
leandro-lisa <leandro.lisa@uala.com.ar>
2023-09-21 00:16:26 -0300

    -

---
+++ ProyectoFinal/routes/cart.router.js
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import { cartManager } from '../CartManager.js';
+
+const router = Router();
+
+router.get('/',async (req,res)=>{
+    try {
+    const queryObj = req.query;
+    const carts = await cartManager.getCarts(queryObj);
+    res.status(200).json({message:'Carts found', carts: carts})
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.get('/:cid',async (req,res)=>{
+    try {
+    const {cid} = req.params;
+    const response = await cartManager.getCartById(cid);
+    if(!response)
+        res.status(404).json({message:response});
+    res.status(200).json({message:'Cart found', cart: response})
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.post('/',async (req,res)=>{
+    try {
+    const response = await cartManager.createCart();
+    res.status(200).json({message:response});
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.post('/:cid/product/:pid',async (req,res)=>{
+    try {
+    const {cid} = req.params;
+    const {pid} = req.params;
+    const response = await cartManager.addProduct(cid,pid);
+    res.status(200).json({message:response});
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
+export default router;
